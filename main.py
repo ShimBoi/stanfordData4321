bd03773a901dc6027ff5b0391214b874e4df962c
HRNAE <148918405+HRNAE@users.noreply.github.com>
2024-08-04 23:34:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -186,6 +186,7 @@ class GradCAM:
         one_hot = torch.zeros((1, self.model.fc.out_features), dtype=torch.float32).to(device)
         one_hot[0][class_idx] = 1
         self.model.zero_grad()
+        # Reshape the forward outputs to match the fully connected layer input
         output = self.model.fc(self.forward_relu_outputs.view(self.forward_relu_outputs.size(0), -1))
         output.backward(gradient=one_hot, retain_graph=True)
         grads = self.gradients[0]
