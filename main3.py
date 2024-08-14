6b9394485f7f46745377f07d02aea376be3a4381
HRNAE <148918405+HRNAE@users.noreply.github.com>
2024-08-13 22:34:58 -0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -188,7 +188,7 @@ def objective(trial):
     return accuracy
 
 study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=8)
+study.optimize(objective, n_trials=2)
 
 best_params = study.best_params
 print("Best parameters found by Optuna:", best_params)
@@ -201,7 +201,7 @@ optimizer = optim.SGD(net.parameters(), lr=best_lr, momentum=best_momentum)
 # Define loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 
-for epoch in range(25):  # Adjust epoch count as needed
+for epoch in range(7):  # Adjust epoch count as needed
     net.train()
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
