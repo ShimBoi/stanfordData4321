a59cab97cc332b207022dd380c8a648f60f63c9c
HRNAE <148918405+HRNAE@users.noreply.github.com>
2024-08-15 00:10:37 -0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -147,7 +147,7 @@ def objective(trial):
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum)
     criterion = nn.CrossEntropyLoss()
 
-    for epoch in range(1):  # Fewer epochs for faster optimization
+    for epoch in range(3):  # Fewer epochs for faster optimization
         net.train()
         running_loss = 0.0
         for inputs, labels in train_loader:
